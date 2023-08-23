13dcaf489b7e9b7b9128713e1ebca53e503deff4
Mike Lewis <mike.lewis@retool.com>
2023-08-22 17:00:57 -0700

    Makign accessor public

--- index.d.ts
+++ index.d.ts
@@ -3,9 +3,9 @@
 // Definitions by: Sasha Grin <https://github.com/tup1tsa>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-type Listener = (newValue: any, oldValue: any, url: string) => any;
+export type Listener = (newValue: any, oldValue: any, url: string) => any;
 
-interface Accessor {
+export interface Accessor {
     /**
      * Invokes set if value is present and get otherwise
      * https://github.com/bevacqua/local-storage#lskey-value
